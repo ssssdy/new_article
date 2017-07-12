976b403a44333363b7507953962d750fdecca152
Your Name <695603546@qq.com>
2017-07-12 21:59:45 +0800

    ss

---
+++ test_index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<?php
+include "./helpers/global_helper.php";
+include "./cache/base_cache.class.php";
+include "./cache/news_comment_cache.php";
+?>
+</body>
+</html>
